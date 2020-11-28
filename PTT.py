a19f7a4dcae707ce6f18411fe1c261427fff6864
yuhaoliao <h770320@gmail.com>
2020-11-28 10:53:28 +0800

    1. 解決 18 按鈕問題 2. 若已經是第一頁讓程式不出錯誤

--- PTT.py
+++ PTT.py
@@ -1,25 +1,65 @@
 import requests
+import numpy as np
 from bs4 import BeautifulSoup
 
-url = 'https://www.ptt.cc/bbs/Food/index.html'
+keyword ='swift'
+isShowNowUrl = False
+#版名
+name = 'Car'
 domain = 'https://www.ptt.cc'
+apiurl = f'/bbs/{name}/index.html'
+over18Url = f'{domain}/ask/over18?from=%2Fbbs%2F{name}%2Findex.html'
+url = f'{domain}{apiurl}'
 
-for times in range(2):
-    res = requests.get(url)
-    #解析模式(html.parser)
-    soup = BeautifulSoup(res.text,'html.parser')
-    #爬取 class = 'title'下的 超連結
-    articles = soup.select('.title a')
+#回傳 List 中 包含 text 的第一筆
+def FirstByText(arr,text):
+    for item in arr:
+        if text in str(item):
+            return item
+   
+#18禁 Button
+def over18Chick(r):
+    r.post(over18Url, payload)#取得頁面資訊   
 
-    for each_title in articles:
-            href = each_title['href']
-            print(each_title.text,f'{domain}{href}')
+def GetLastUrl(soup):
+    u = soup.select(".btn.wide") #相關 a標籤
+    tag = FirstByText(u,'上頁') #取得包含相關字串的 Tag
+    
+    lastUrl = tag.get("href")
+    
+    if lastUrl is None: #找不到按鈕跳出不執行
+        return None
+    
+    return f'{domain}{lastUrl}'
 
-    #獲取上一頁連結
-    paging = soup.select('.btn.wide')
-    if len(paging) > 0:
-        href = paging[1]['href']
-        lastUrl = f'{domain}{href}'
-        url = lastUrl
-    else:
+r = requests.Session()
+payload ={
+    "from":apiurl,
+    "yes":"yes"
+}
+
+for i in range(100): #往上爬3頁
+    if isShowNowUrl:
+        print(f"本頁的URL為 {url}")
+    over18Chick(r)
+    
+    response = r.get(url)
+    
+    soup = BeautifulSoup(response.text,"html.parser")
+    
+    sel = soup.select("div.title a") #標題
+    
+    if keyword != '':
+        sel = filter(lambda x: True if keyword.lower() in str(x).lower() else False ,sel)
+        
+    for s in sel: #印出網址跟標題
+        print(f'{domain}{s["href"]}',s.text)
+    
+    if isShowNowUrl:
+        print('')
+    
+    url = GetLastUrl(soup)#上一頁的網址
+    
+    if url is None: #沒有上一頁網址跳出迴圈
+        print('-----------找不到上一頁的路徑或已經到第一頁-----------')
         break
