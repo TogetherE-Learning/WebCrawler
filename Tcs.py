47d32e1b41570263904ee24bfb9dc0d60b6141ef
Tony <h770320@gmail.com>
2020-12-08 09:04:13 +0800

    最新版本

--- Tcs.py
+++ Tcs.py
@@ -1,22 +1,44 @@
 from selenium import webdriver as wd
-import json
 from selenium.webdriver.support.ui import Select
 from bs4 import BeautifulSoup
+import datetime
+import calendar
+from datetime import timedelta
+import csv
 
+def add_months(sourcedate, months):
+    month = sourcedate.month - 1 + months
+    year = sourcedate.year + month // 12
+    month = month % 12 + 1
+    day = min(sourcedate.day, calendar.monthrange(year,month)[1])
+    return datetime.date(year, month, day)
+
+def getnowdate(sourcedate):
+    month = sourcedate.month
+    year = sourcedate.year
+    day = sourcedate.day
+    return datetime.date(year, month, day)
+
+today = getnowdate(datetime.datetime.now())
 startTime = '20201101'
 endTime = '20201201'
-#usr = '11415 劉昱揚'
-usr = '13352 廖裕豪'
+dateFormatter = "%Y%m%d"
+
+if(today.day < 16):
+    today = add_months(today,-1)
+    startTime = datetime.datetime.strftime(time,dateFormatter)
+    time = add_months(today,1) - timedelta(today.day)
+    endTime = datetime.datetime.strftime(time,dateFormatter)
+else:
+    time = today - timedelta(today.day-1)
+    startTime = datetime.datetime.strftime(time,dateFormatter)
+    time = add_months(today,1) - timedelta(today.day)
+    endTime = datetime.datetime.strftime(time,dateFormatter)
+
 driver = wd.Chrome()
 driver.get('http://chap02/tcs/Forms/TCSReport.aspx?FORM_ID=100226&PRIV_ID=103&FORM_NME=%e5%b7%a5%e4%bd%9c%e6%97%a5%e8%aa%8c%e8%bc%b8%e5%85%a5')
 driver.find_element_by_name("txtStartDate").send_keys(startTime)
 driver.find_element_by_name("txtEndDate").send_keys(endTime)
-
-select = Select(driver.find_element_by_name('txtUSR_ID'))
-
-for index in range(len(select.options)):
-    if(select.options[index].text == usr):
-        select.select_by_index(index)
 
 driver.find_element_by_name("btnQuery").click()
 
@@ -65,6 +87,13 @@ for item in array:
         if(projectName not in resultDic.keys()):
             resultDic[projectName] = float(0)
         resultDic[projectName] = resultDic[projectName] + workingHours
+# 開啟輸出的 CSV 檔案
+with open('output.csv', 'w', newline='') as csvfile: 
+    # 建立 CSV 檔寫入器
+    writer = csv.writer(csvfile)
+    for item in resultDic:
+        print(f'{item} {resultDic[item]}')
+        # 寫入一列資料
+        writer.writerow([f"'{item}", f"{resultDic[item]}"])
+
         
-for item in resultDic:
-    print(f'{item} {resultDic[item]}')
